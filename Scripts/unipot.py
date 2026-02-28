3b0866b74d02610e7a8add9b7c76f2f59b65b795
Linda Amber <inf-20-2025@bioinf-serv2.cob.lu.se>
2026-02-28 18:02:00 +0100

    find more birds

--- Scripts/unipot.py
+++ Scripts/unipot.py
@@ -2,19 +2,19 @@ import requests
 import time
 
 # Read your abbreviations
-with open("blastx_unique.txt") as f:
+with open("blastp_unique.txt") as f:
     abbrevs = [line.split()[1] for line in f if line.strip()]
 
 birds = []
 
 for abbrev in abbrevs:
-    url = f"https://rest.uniprot.org/taxonomy/search?query={abbrev}&fields=lineage"
+    url = f"https://rest.uniprot.org/taxonomy/search?query=mnemonic:{abbrev}&fields=lineage,mnemonic"
     response = requests.get(url)
     data = response.json()
     
     if data.get("results"):
         lineage = str(data["results"][0])
-        if "Aves" in lineage:
+        if "Aves" in lineage: # change for lineage
             birds.append(abbrev)
             print(f"{abbrev} is a bird!")
     
