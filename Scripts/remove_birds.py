4815db8241d1a197bc281dcd898366991299aeb6
Linda Amber <inf-20-2025@bioinf-serv2.cob.lu.se>
2026-03-02 17:12:19 +0100

    add python script that removes bird contigs

---
+++ Scripts/remove_birds.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 11:54:15 2026
+
+Usage: remove_birds.py blastp_birds.txt <faa or fna> <output_name>
+@author: lindawinnicki
+"""
+
+import sys
+
+sys.argv[1] = query_path
+sys.argv[2] = fasta_path
+sys.argv[3] = output_path
+
+# query_path = "/Users/lindawinnicki/Documents/Lu_binp29/Malaria/blastp_birds.txt"
+
+query_list = []
+
+with open(query_path, "r") as queries:
+    for line in queries:
+        if line.startswith("#"):
+            continue
+        query = line.partition(" ")[0] # only get the query
+        query_list.append(query)
+
+
+# fasta_path = "/Users/lindawinnicki/Documents/Lu_binp29/Malaria/gff.faa"
+
+contig_list = {}
+
+with open(fasta_path, "r") as contigs:
+    for line in contigs:
+        if line.startswith(">"):
+            contig = line.split("\t")[2].partition("=")[2] # only get contig
+            c_query = line.split("\t")[0].strip(">") # get the query
+            contig_list[c_query] = contig
+
+bird_contigs = []
+
+for k, v in contig_list.items():
+    if k in query_list:
+        bird_contigs.append(v)
+
+non_bird_contigs = {}
+
+for k, v in contig_list.items():
+    if v not in bird_contigs:
+        non_bird_contigs[k] = v
+
+
+# make a new file with non_bird contigs
+
+output = ""
+# output_path = "/Users/lindawinnicki/Documents/Lu_binp29/Malaria/gff_no_birds.faa"
+
+print_next = False # flag to print sequence too
+with open(contig_path, "r") as contigs:
+    for line in contigs:
+        if line.startswith(">"):
+            contig = line.split("\t")[2].partition("=")[2] # only get contig
+            if contig in non_bird_contigs.values():
+                output += line
+                print_next = True
+            else:
+                print_next = False
+        elif print_next: # is true
+            output += line
+
+with open(output_path, "w") as nobird:
+    nobird.write(output)
